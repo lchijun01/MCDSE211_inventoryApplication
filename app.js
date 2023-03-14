83d34aeb85e4be06c20dcfe80b011f74400dc513
Lai Chi Jun <chinjun@outlook.com>
2023-03-14 13:17:44 +0800

    add require and setup for not resubmmsion when refresh the page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'))
+
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
@@ -17,6 +18,7 @@ const pool = mysql.createPool({
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res) {
     res.render('index');
@@ -90,6 +92,129 @@ app.post('/expenses-record',upload.single('file'),  urlencodedParser, function(r
       }
     });
   });
+
+  app.get('/expenses-record', function(req, res){
+    res.render('expenses-record');
+});
+
+
+//for sales-sell page
+app.get('/sell', function(req, res){
+  res.render('sell');
+});
+//for sales-sell page
+app.post('/sell',upload.single('file'),  urlencodedParser, function(req, res){
+    const { date, invoice_no, category, bank, name, amount, detail } = req.body;
+
+    // Get the filename from the request
+    const filename = req.file ? req.file.filename : 'N/A';
+  
+    // Insert the form data into MySQL
+    pool.query('INSERT INTO sell (Date, Invoice_No, Category, Bank, Name, Amount, Detail, File) VALUES (?, ?, ?, ?, ?, ?, ?, ifnull(?, "N/A"))', [date, invoice_no, category, bank, name, amount, detail, filename], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error saving form data');
+      } else {
+        console.log(req.body);
+        res.render('expenses-record');
+      }
+    });
+  });
+
+//for sales-payment break
+app.get('/sales-paymentbreak', function(req, res){
+  res.render('sales-paymentbreak');
+});
+
+app.post('/sales-paymentbreak',upload.single('file'),  urlencodedParser, function(req, res){
+    const { date, invoice_no, bank, amount, remarks } = req.body;
+
+    // Get the filename from the request
+    const filename = req.file ? req.file.filename : 'N/A';
+
+    // Insert the form data into MySQL
+    pool.query('INSERT INTO sales_paymentbreakdown (Date, Invoice_No, Bank, Amount, Remarks, File) VALUES (?, ?, ?, ?, ?, ifnull(?, "N/A"))', [date, invoice_no, bank, amount, remarks, filename], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error saving form data');
+      } else {
+        console.log(req.body);
+        req.body.reset
+        res.render('sales-paymentbreak', { successMessage: 'Form submitted successfully' });
+      }
+    });
+  });
+
+  //for buy-payment break
+app.get('/buy-paymentbreak', function(req, res){
+  res.render('buy-paymentbreak');
+});
+
+app.post('/buy-paymentbreak',upload.single('file'),  urlencodedParser, function(req, res){
+    const { date, invoice_no, bank, amount, remarks } = req.body;
+
+    // Get the filename from the request
+    const filename = req.file ? req.file.filename : 'N/A';
+
+    // Insert the form data into MySQL
+    pool.query('INSERT INTO purchase_paymentbreakdown (Date, Invoice_No, Bank, Amount, Remarks, File) VALUES (?, ?, ?, ?, ?, ifnull(?, "N/A"))', [date, invoice_no, bank, amount, remarks, filename], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error saving form data');
+      } else {
+        console.log(req.body);
+        req.body.reset
+        res.render('buy-paymentbreak', { successMessage: 'Form submitted successfully' });
+      }
+    });
+  });
+//for sales-balance check
+app.get('/sales-balancecheck', function(req, res){
+  res.render('sales-balancecheck');
+});
+
+app.post('/sales-balancecheck',upload.single('file'),  urlencodedParser, function(req, res){
+    const { date, invoice_no, category, bank, name, amount, detail } = req.body;
+
+    // Get the filename from the request
+    const filename = req.file ? req.file.filename : 'N/A';
+  
+    // Insert the form data into MySQL
+    pool.query('INSERT INTO sell (Date, Invoice_No, Category, Bank, Name, Amount, Detail, File) VALUES (?, ?, ?, ?, ?, ?, ?, ifnull(?, "N/A"))', [date, invoice_no, category, bank, name, amount, detail, filename], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error saving form data');
+      } else {
+        console.log(req.body);
+        res.render('sales-balancecheck');
+      }
+    });
+
+  });
+
+  //for sales-invoice generator
+app.get('/invoice-generator', function(req, res){
+  res.render('invoice-generator');
+});
+
+app.post('/invoice-generator',upload.single('file'),  urlencodedParser, function(req, res){
+    const { date, invoice_no, category, bank, name, amount, detail } = req.body;
+
+    // Get the filename from the request
+    const filename = req.file ? req.file.filename : 'N/A';
+  
+    // Insert the form data into MySQL
+    pool.query('INSERT INTO sell (Date, Invoice_No, Category, Bank, Name, Amount, Detail, File) VALUES (?, ?, ?, ?, ?, ?, ?, ifnull(?, "N/A"))', [date, invoice_no, category, bank, name, amount, detail, filename], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error saving form data');
+      } else {
+        console.log(req.body);
+        res.render('invoice-generator');
+      }
+    });
+  });
+
 
 app.get('/profile/:name', function(req, res){
     res.render('profile', {person: req.params.name});
